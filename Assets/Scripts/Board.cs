ba01dc8d4e5a4ef7d35fc26d24cb63c1fa68ff29
AkbulutSemiha <30518138+AkbulutSemiha@users.noreply.github.com>
2025-01-08 20:12:18 +0300

    v.1
    Http request ile kelime ve feedback bilgilerine göre ui da gösterimi

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Text;
+using Newtonsoft.Json;
 
 [DefaultExecutionOrder(-1)]
+[System.Serializable]  
+public class Guess  
+{  
+    public string guess;  
+    public List<int> feedback;  
+}  
+
+[System.Serializable]  
+public class Guesses  
+{  
+    public List<Guess> guesses;  
+}  
+[System.Serializable]  
+public class PredictionResponse  
+{  
+    public string prediction;  
+}  
 public class Board : MonoBehaviour
 {
-    private static readonly KeyCode[] SUPPORTED_KEYS = new KeyCode[] {
-        KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F,
-        KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L,
-        KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R,
-        KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X,
-        KeyCode.Y, KeyCode.Z,
-    };
-
     private static readonly string[] SEPARATOR = new string[] { "\r\n", "\r", "\n" };
 
     private Row[] rows;
@@ -20,7 +34,11 @@ public class Board : MonoBehaviour
     private string[] solutions;
     private string[] validWords;
     private string word;
-
+    private string guess;
+    Guesses guesses = new Guesses  
+    {  
+        guesses = new List<Guess>()  
+    };  
     [Header("Tiles")]
     public Tile.State emptyState;
     public Tile.State occupiedState;
@@ -42,43 +60,108 @@ public class Board : MonoBehaviour
     {
         LoadData();
         NewGame();
+        StartProcessing();
     }
+     void AddGuess(Guesses guesses, string guessWord, List<int> feedback)  
+    {  
+        Guess newGuess = new Guess  
+        {  
+            guess = guessWord,  
+            feedback = feedback  
+        };  
+        guesses.guesses.Add(newGuess);  
+    }  
+    IEnumerator PostDataToFastAPI()
+    {
+        Debug.Log("PostDataToFastAPI");  
+        // Gönderilecek veri (JSON formatında)  
+        string jsonData = JsonConvert.SerializeObject(guesses, Formatting.Indented);   
+        Debug.Log("Gönderilen JSON: " + jsonData);  
 
+        // UnityWebRequest ile HTTP isteği oluştur  
+        using (UnityWebRequest request = new UnityWebRequest("http://127.0.0.1:8000/postfeedback/", "POST"))  
+        {  
+            // JSON verisini gönderilecek veri olarak ayarla  
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);  
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);  
+            request.downloadHandler = new DownloadHandlerBuffer();  
+            request.SetRequestHeader("Content-Type", "application/json");  
+
+            // İsteği gönder ve yanıtı bekle  
+            yield return request.SendWebRequest();  
+
+            // Yanıt kontrolü  
+            if (request.result == UnityWebRequest.Result.Success)  
+            {  
+                Debug.Log("Başarılı yanıt: " + request.downloadHandler.text);  
+                // Yanıtı işleme  
+                ProcessResponse(request.downloadHandler.text);  
+            }  
+            else  
+            {  
+                Debug.LogError("Hata oluştu: " + request.error);  
+            }  
+        } 
+    }
+    private void ProcessResponse(string jsonResponse)  
+    {  
+        // Yanıtı işleyin (örneğin, tahmin kelimesini alın)  
+        var predictionResponse = JsonConvert.DeserializeObject<PredictionResponse>(jsonResponse);  
+        Debug.Log("Tahmin: " + predictionResponse.prediction);  
+        guess = predictionResponse.prediction;
+        PerformOtherOperations(predictionResponse.prediction);  
+    }  
+
+    private void PerformOtherOperations(string prediction)  
+    {  
+        // Yanıt alındıktan sonra yapılacak diğer işlemler  
+        Debug.Log("Performing other operations with prediction: " + prediction);  
+        guess = prediction;
+
+    } 
     private void LoadData()
     {
-        TextAsset textFile = Resources.Load("official_wordle_common") as TextAsset;
-        solutions = textFile.text.Split(SEPARATOR, System.StringSplitOptions.None);
-
+        //TextAsset textFile = Resources.Load("official_wordle_common") as TextAsset;
+        //solutions = textFile.text.Split(SEPARATOR, System.StringSplitOptions.None);
+        TextAsset textFile;
         textFile = Resources.Load("official_wordle_all") as TextAsset;
         validWords = textFile.text.Split(SEPARATOR, System.StringSplitOptions.None);
+        solutions= validWords;
     }
 
     public void NewGame()
     {
         ClearBoard();
+        SetTargetWord();
         SetRandomWord();
-
+        //guess= "TARİK";
         enabled = true;
     }
 
     public void TryAgain()
     {
         ClearBoard();
-
+        NewGame();
+        StartProcessing();
         enabled = true;
     }
-
-    private void SetRandomWord()
+    private void SetTargetWord()
     {
         word = solutions[Random.Range(0, solutions.Length)];
-        word = word.ToLower().Trim();
+        //word = word.ToLower().Trim();
+        Debug.Log("HEDEF: "+word);
+    }
+    private void SetRandomWord()
+    {
+        guess = solutions[Random.Range(0, solutions.Length)];
+        //guess = guess.ToLower().Trim();
+        Debug.Log("Tahmin: "+guess);
     }
 
     private void Update()
     {
-        Row currentRow = rows[rowIndex];
-
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        
+        /*if (Input.GetKeyDown(KeyCode.Backspace))
         {
             columnIndex = Mathf.Max(columnIndex - 1, 0);
             currentRow.tiles[columnIndex].SetLetter('\0');
@@ -103,19 +186,63 @@ public class Board : MonoBehaviour
                     break;
                 }
             }
-        }
+        }*/
     }
+    private IEnumerator ProcessGuess()  
+    {  
+        Row currentRow = rows[rowIndex];  
+        if (columnIndex < guess.Length)  
+        {  
+            char letter = guess[columnIndex];  
+            currentRow.tiles[columnIndex].SetLetter(letter);  
+            currentRow.tiles[columnIndex].SetState(occupiedState);  
+            columnIndex++;  
+        }  
+        else  
+        {  
+            Debug.Log("SubmitRow");  
+            AddGuess(guesses, guess, SubmitRow(currentRow));  
+            yield return StartCoroutine(PostDataToFastAPI()); // POST isteğini bekle  
 
-    private void SubmitRow(Row row)
+            rowIndex++; // Bir sonraki satıra geç  
+            columnIndex = 0; // Kolon indeksini sıfırla  
+
+            if (rowIndex >= rows.Length)  
+            {  
+                enabled = false;  
+            }  
+            /*else  
+            {   
+                SetRandomWord(); // Yeni bir kelime ayarla  
+            } */ 
+        }  
+
+        yield return new WaitForSeconds(0.5f); // 0.5 saniye bekle  
+
+        if (rowIndex < rows.Length)  
+        {  
+            StartCoroutine(ProcessGuess());  
+        }  
+    } 
+
+    private void StartProcessing()  
+    { 
+        Debug.Log("startprocess");
+        StartCoroutine(ProcessGuess());  
+    }  
+
+    private List<int> SubmitRow(Row row)
     {
-        if (!IsValidWord(row.word))
+        List<int> feedback = new List<int>();
+        Debug.Log("tiles len: "+row.tiles.Length);
+        /*if (!IsValidWord(row.word))
         {
             invalidWordText.SetActive(true);
             return;
-        }
+        }*/
 
         string remaining = word;
-
+       
         // Check correct/incorrect letters first
         for (int i = 0; i < row.tiles.Length; i++)
         {
@@ -123,6 +250,7 @@ public class Board : MonoBehaviour
 
             if (tile.letter == word[i])
             {
+                feedback.Add(2);
                 tile.SetState(correctState);
 
                 remaining = remaining.Remove(i, 1);
@@ -130,7 +258,12 @@ public class Board : MonoBehaviour
             }
             else if (!word.Contains(tile.letter))
             {
+                feedback.Add(0);
                 tile.SetState(incorrectState);
+            }
+            else
+            {
+                feedback.Add(1);
             }
         }
 
@@ -160,12 +293,13 @@ public class Board : MonoBehaviour
             enabled = false;
         }
 
-        rowIndex++;
+        //rowIndex++;
         columnIndex = 0;
 
         if (rowIndex >= rows.Length) {
             enabled = false;
         }
+        return feedback;
     }
 
     private bool IsValidWord(string word)
